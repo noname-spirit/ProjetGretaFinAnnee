1bab97cbe39231a6f7e0da3a3d44e379f3335e5d
noname <bonjour@noname-spirit.com>
2025-05-19 15:57:39 +0200

    contact_page

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact', methods: ['GET'])]
+    public function index(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('front/contact.html.twig');
+    }
+
+    #[Route('/contact/send', name: 'contact_send', methods: ['POST'])]
+    public function send(Request $request, MailerInterface $mailer)
+    {
+        $data = $request->request;
+
+        $email = (new Email())
+            ->from($data->get('email'))
+            ->to('[mail client pro]') // Remplace par l'email de ta cliente
+            ->subject('New Contact Request from ' . $data->get('first_name') . ' ' . $data->get('last_name'))
+            ->text(sprintf(
+                "Name: %s %s\nEmail: %s\nPhone: %s\n\nComments:\n%s\n\nServices selected:\n%s",
+                $data->get('first_name'),
+                $data->get('last_name'),
+                $data->get('email'),
+                $data->get('phone'),
+                $data->get('comments'),
+                implode(", ", $data->all('services') ?? [])
+            ));
+
+        $mailer->send($email);
+
+        $this->addFlash('success', 'Your message has been sent!');
+        return $this->redirectToRoute('contact');
+    }
+}
